f5420541c7f260c9c1cacc7be10498d311bd1faa
y-2j <etozhedafomenko@gmail.com>
2019-10-22 17:49:50 +0400

    Выполнение tabs

---
+++ tabs/task.js
@@ -0,0 +1,23 @@
+let counter = 0;
+let tabActive = 0;
+let tabContentActive = 0;
+const menu = document.body.querySelectorAll(".tab");
+for (let i = 0; i < menu.length; i++) {
+    menu[i].addEventListener("click", function () {
+        this.classList.add("tab_active");
+        const content = document.body.querySelectorAll('.tab__content');
+        if (counter >= 1) {
+            tabActive.remove("tab_active");
+            tabContentActive.remove("tab__content_active");
+            if (counter > 3) {
+                counter = 0;
+                tabContentActive = 0;
+            }
+
+        }
+        content[i].classList.add('tab__content_active');
+        tabContentActive = content[i].classList;
+        tabActive = this.classList;
+        counter += 1;
+    })
+}
